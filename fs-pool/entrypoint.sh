38e8b864a0f363016cd7b41554014dc7ea1dac29
Ollie Ford <dev@ojford.com>
2018-01-28 13:52:50 +0000

    Refactor disk mount dir to env var

--- fs-pool/entrypoint.sh
+++ fs-pool/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/mergerfs -d -o defaults,allow_other,use_ino /mnt/\* "$POOL_MOUNTPOINT"
+/usr/bin/mergerfs -d -o defaults,allow_other,use_ino "${DISK_MOUNTS_DIR}/*" "$POOL_MOUNTPOINT"
